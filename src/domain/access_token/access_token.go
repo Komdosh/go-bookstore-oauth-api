bebf0faeaed63caf36b0012ae18479f25d987f84
Andrey Tabakov <komdosh@yandex.ru>
2021-01-14 12:45:50 +0300

    feat(oauth): add access token request

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,13 +1,17 @@
 package access_token
 
 import (
+	"fmt"
 	"github.com/Komdosh/go-bookstore-oauth-api/src/utils/errors_utils"
+	"github.com/Komdosh/go-bookstore-users-api/utils/crypto_utils"
 	"strings"
 	"time"
 )
 
 const (
-	expirationTime = 24
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grantTypeClientCredentials = "client_credentials"
 )
 
 type AccessToken struct {
@@ -17,8 +21,39 @@ type AccessToken struct {
 	Expires     int64  `json:"expires"`
 }
 
-func GetNewAccessToken() AccessToken {
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+
+	// grant_type: password
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (at *AccessTokenRequest) Validate() *errors_utils.RestErr {
+	switch at.GrantType {
+	case grantTypePassword:
+		at.Username = strings.TrimSpace(at.Username)
+		at.Password = strings.TrimSpace(at.Password)
+		if at.Username == "" {
+			return errors_utils.NewBadRequestError("invalid username")
+		}
+		if at.Password == "" {
+			return errors_utils.NewBadRequestError("invalid password")
+		}
+		break
+	case grantTypeClientCredentials:
+		return errors_utils.NewBadRequestError("client_credentials grant type does not supported yet")
+	default:
+		return errors_utils.NewBadRequestError("invalid grant_type parameter")
+	}
+
+	return nil
+}
+
+func GetNewAccessToken(userId int64) AccessToken {
 	return AccessToken{
+		UserId:  userId,
 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
@@ -44,4 +79,8 @@ func (at AccessToken) IsExpired() bool {
 	now := time.Now().UTC()
 	expirationTime := time.Unix(at.Expires, 0)
 	return expirationTime.Before(now)
+}
+
+func (at *AccessToken) Generate() {
+	at.AccessToken = crypto_utils.GetSha1(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
